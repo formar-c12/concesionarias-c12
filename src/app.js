5115e7141216d5f0b1aa0383dfd5a35e70aabbfd
Jona <retratos.jonatan@gmail.com>
2021-11-12 17:28:06 -0300

    Actualiza

--- src/app.js
+++ src/app.js
@@ -5,9 +5,12 @@ const PORT = 3000
 /* Enrutadores */
 let indexRouter = require('./routes/indexRouter')
 let marcasRouter = require('./routes/marcasRouter')
+let sucursalesRouter = require('./routes/sucursalesRouter')
 
+/* Middlewares de rutas */
 app.use('/', indexRouter)
 app.use('/marcas', marcasRouter)
+app.use('/sucursales', sucursalesRouter)
 
 
 app.listen(PORT, () => console.log("Servidor levantado"))
